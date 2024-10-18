19996b51b0317a891dd22c125912da9c088c8df6
JesseDolfin <jesse.dolfin@gmail.com>
2024-10-18 16:42:21 +0200

    cleanup of reinforcement learning code

--- src/q_learning/src/CoLearnEnvironment.py
+++ src/q_learning/src/CoLearnEnvironment.py
@@ -1,51 +1,51 @@
 #!/usr/bin/env python3
+
 from gymnasium import Env
 from gymnasium.spaces import Discrete
-import threading
 import rospy
 import rosgraph
-from co_learning_messages.msg import secondary_task_message,hand_pose
+from co_learning_messages.msg import secondary_task_message, hand_pose
 from std_msgs.msg import Bool
-import time
+
 
 class CoLearn(Env):
-    def __init__(self):
-        '''
-        Defines action space containing 8 actions (0-7)
-        - Move to home                      : 0, 0
-        - Initiate handover immediately     : 1, 1
-        - Wait for state_change             : 1, 2
-        - Go to Serve                       : 2, 3
-        - Go to Drop                        : 2, 4
-        - Open hand                         : 3, 5
-        - Open hand partially               : 3, 6
-        - Close hand                        : 3, 7
-        Where the first number indicates the phase
+    """
+    Defines an environment for reinforcement learning with the following:
 
-        
-        Defines an observation space containing 17 states (0-16)
-        - Home                                          : 0, 0
-        - (Immediately, hand in workspace)              : 1, 1
-        - (Immediately, hand not in workspace)          : 1, 2
-        - (wait for state change, hand in workspace)    : 1, 3
-        - (wait for state change, hand not in workspace): 1, 4
-        - (Hand_h: serve, robot: serve)                 : 2, 5
-        - (Hand_h: serve, robot: drop)                  : 2, 6
-        - (Hand_h: drop, robot: serve)                  : 2, 7
-        - (hand_h: drop, robot: drop)                   : 2, 8
-        - (Hand_h: unknown, robot: serve)               : 2, 9
-        - (Hand_h: unknown, robot: drop)                : 2, 10
-        - (Human_input, Hand open)                      : 3, 11
-        - (Human_input, Hand partial)                   : 3, 12
-        - (Human_input, Hand close)                     : 3, 13
-        - (No human_input, Hand open)                   : 3, 14
-        - (No human_input, Hand partial)                : 3, 15
-        - (No human_input, Hand close)                  : 3, 16
-        '''
- 
+    - **Action Space** (8 actions):
+        - Move to home: 0
+        - Initiate handover immediately: 1
+        - Wait for state change: 2
+        - Go to Serve: 3
+        - Go to Drop: 4
+        - Open hand: 5
+        - Open hand partially: 6
+        - Close hand: 7
+
+    - **Observation Space** (17 states):
+        - Home: 0
+        - (Immediately, hand in workspace): 1
+        - (Immediately, hand not in workspace): 2
+        - (Wait for state change, hand in workspace): 3
+        - (Wait for state change, hand not in workspace): 4
+        - (Hand_h: serve, robot: serve): 5
+        - (Hand_h: serve, robot: drop): 6
+        - (Hand_h: drop, robot: serve): 7
+        - (Hand_h: drop, robot: drop): 8
+        - (Hand_h: unknown, robot: serve): 9
+        - (Hand_h: unknown, robot: drop): 10
+        - (Human input, hand open): 11
+        - (Human input, hand partial): 12
+        - (Human input, hand close): 13
+        - (No human input, hand open): 14
+        - (No human input, hand partial): 15
+        - (No human input, hand close): 16
+    """
+
+    def __init__(self):
         self.action_size = 8
         self.action_space = Discrete(self.action_size)
-        
+
         self.observation_size = 17
         self.observation_space = Discrete(self.observation_size)
 
@@ -53,20 +53,14 @@ class CoLearn(Env):
         self.phase_size = 4
 
         self.state_size = 17
-        self.state = 0 
+        self.state = 0
 
         self.reward = 0
 
         self.max_episode_length = 100
-        self.episode_length = self.max_episode_length 
+        self.episode_length = self.max_episode_length
 
-        self.info = {'valid':None,
-                     'phase':0,
-                     'break':False}
-        
-     
-        self.condition = threading.Condition()
-        self.relevant_message = None
+        self.info = {'valid': None, 'phase': 0}
 
         self.handover_successful = 0
         self.human_input = False
@@ -74,89 +68,116 @@ class CoLearn(Env):
 
         self.terminated = False
 
-        self.hand_open = False
-
         self.type = 'none'
 
-        self.initialise_ros()
+        self.initialize_ros()
 
     def status_callback(self, msg):
-        self.handover_successful= msg.handover_successful
+        self.handover_successful = msg.handover_successful
         self.time_left = msg.time_left
 
-    def hand_pose_callback(self,msg):
+    def hand_pose_callback(self, msg):
         self.orientation = msg.orientation
 
     def human_input_callback(self, msg):
         self.human_input = msg.data
 
-    def initialise_ros(self):
+    def initialize_ros(self):
         self.ros_running = rosgraph.is_master_online()
         if self.ros_running:
-            rospy.Subscriber('Task_status',secondary_task_message,self.status_callback)
-            rospy.Subscriber('hand_pose',hand_pose,self.hand_pose_callback)
-            rospy.Subscriber('human_input',Bool,self.human_input_callback)
-            self.rate=rospy.Rate(1)
             try:
                 rospy.init_node('Environment', anonymous=True)
-            except:
-                rospy.logwarn("Cannot initialize node 'Environment' as it has already been initialized at the top level as a ROS node.")
+            except rospy.exceptions.ROSException:
+                rospy.logwarn(
+                    "Cannot initialize node 'Environment' as it has already been initialized."
+                )
+            rospy.Subscriber('Task_status', secondary_task_message, self.status_callback)
+            rospy.Subscriber('hand_pose', hand_pose, self.hand_pose_callback)
+            rospy.Subscriber('human_input', Bool, self.human_input_callback)
         else:
             print("ROS is offline! Environment proceeds in offline mode")
 
     def check_phase_transition(self, action):
-        if self.phase == 0: return action in [1, 2]
-        elif self.phase == 1: return action in [3, 4]
-        elif self.phase == 2: return action in [5, 6, 7]
-        elif self.phase == 3: return action == 5
-        else: return False
-        
+        if self.phase == 0:
+            return action in [1, 2]
+        elif self.phase == 1:
+            return action in [3, 4]
+        elif self.phase == 2:
+            return action in [5, 6, 7]
+        elif self.phase == 3:
+            return action == 5
+        else:
+            return False
+
     def update_phase(self):
-        if self.state == 0: self.phase = 0
-        elif self.state in [1,2,3,4]: self.phase = 1
-        elif self.state in [5,6,7,8,9,10]: self.phase = 2
-        elif self.state in [11,12,13,14,15,16]: self.phase = 3
-        else: rospy.logwarn(f"No valid phase found, phase is:{self.phase}")
+        if self.state == 0:
+            self.phase = 0
+        elif self.state in [1, 2, 3, 4]:
+            self.phase = 1
+        elif self.state in [5, 6, 7, 8, 9, 10]:
+            self.phase = 2
+        elif self.state in [11, 12, 13, 14, 15, 16]:
+            self.phase = 3
+        else:
+            rospy.logwarn(f"No valid phase found, phase is: {self.phase}")
 
     def update_state(self, action):
         if self.state == 0:
-            if action == 1: return 2 if self.orientation == 'None' else 1  # self.orientation == 'None' means that the hand is not in the workspace
-            elif action == 2: return 4 if self.orientation == 'None' else 2
-            
-        elif self.state in [1,2,3,4]:
+            if action == 1:
+                return 2 if self.orientation == 'None' else 1
+            elif action == 2:
+                return 4 if self.orientation == 'None' else 2
+
+        elif self.state in [1, 2, 3, 4]:
             if action == 3:
-                if self.orientation == 'Serve': return 5 
-                elif self.orientation == 'Drop': return 7
-                else: return 9
+                if self.orientation == 'Serve':
+                    return 5
+                elif self.orientation == 'Drop':
+                    return 7
+                else:
+                    return 9
             elif action == 4:
-                if self.orientation == 'Serve': return 6
-                elif self.orientation == 'Drop': return 8
-                else: return 10
+                if self.orientation == 'Serve':
+                    return 6
+                elif self.orientation == 'Drop':
+                    return 8
+                else:
+                    return 10
 
-        elif self.state in [5,6,7,8,9,10]:
+        elif self.state in [5, 6, 7, 8, 9, 10]:
             if self.human_input:
-                if action == 5: return 11
-                elif action == 6: return 12
-                elif action == 7: return 13
+                if action == 5:
+                    return 11
+                elif action == 6:
+                    return 12
+                elif action == 7:
+                    return 13
             else:
-                if action == 5: return 14
-                if action == 6: return 15
-                if action == 7: return 16
+                if action == 5:
+                    return 14
+                elif action == 6:
+                    return 15
+                elif action == 7:
+                    return 16
 
-        elif self.state in [11,12,13,14,15,16]:
-            #if action == 0: return 0
+        elif self.state in [11, 12, 13, 14, 15, 16]:
             if self.human_input:
-                if action == 5: return 11
-                if action == 6: return 12
-                if action == 7: return 13
+                if action == 5:
+                    return 11
+                elif action == 6:
+                    return 12
+                elif action == 7:
+                    return 13
             else:
-                if action == 5: return 14
-                if action == 6: return 15
-                if action == 7: return 16
+                if action == 5:
+                    return 14
+                elif action == 6:
+                    return 15
+                elif action == 7:
+                    return 16
 
-        # If no valid transition is found return the current state
+        # If no valid transition is found, return the current state
         return self.state
-          
 
     def step(self, action=None):
         action = self.action_space.sample() if action is None else action
@@ -173,51 +194,50 @@ class CoLearn(Env):
 
         self.obtain_reward(action)
 
-        self.info['valid'] = valid_transition 
+        self.info['valid'] = valid_transition
         self.info['phase'] = self.phase
 
         return self.state, self.reward, self.terminated, self.info
 
-    def obtain_reward(self,action):
+    def obtain_reward(self, action):
         self.reward = 0
         if self.ros_running:
             if self.previous_phase == 3 and action == 5:
                 # This means that the hand is now open
+                rate = rospy.Rate(5)
                 while self.handover_successful == 0:
-                    # Before continuing wait for confirmation that the handover was either successful or failed
-                    rospy.Rate(5).sleep()
+                    # Wait for confirmation that the handover was successful or failed
+                    rate.sleep()
                     rospy.loginfo_once("Waiting for handover status")
                 if self.handover_successful == 1:
-                    self.reward += self.observation_size * 10 # Reward for finishing is 10 (for each state)
-                    self.reward += self.observation_size * self.time_left * 2 # Dynamic reward based on performance
-
+                    # Reward for successful handover
+                    self.reward += self.observation_size * 10
+                    self.reward += self.observation_size * self.time_left * 2
                 elif self.handover_successful == -1:
-                    self.reward -= self.observation_size * 10 
+                    # Penalty for failed handover
+                    self.reward -= self.observation_size * 10
 
-            if self.type == 'leader' and self.state in [5,7]:
-                self.reward += 10 # robot prefers the state 'serve'
+            if self.type == 'leader' and self.state in [5, 7]:
+                # Robot prefers the state 'serve'
+                self.reward += 10
 
-            if self.previous_state in [12,15] and action in [6,7]:
-                self.reward -= 2 # Incentive not to close the hand
+            if self.previous_state in [12, 15] and action in [6, 7]:
+                # Incentive not to close the hand
+                self.reward -= 2
 
-            if self.previous_state in [13,16] and action == 7:
-                self.reward -= 2 # Incentive not to keep the hand closed
-
+            if self.previous_state in [13, 16] and action == 7:
+                # Incentive not to keep the hand closed
+                self.reward -= 2
         else:
-            self.reward = 0 # OFFLINE REWARD FUNCTION GOES HERE
+            self.reward = 0  # Offline reward function can be implemented here
 
-     
-    
     def reset(self):
         self.state = 0
         self.phase = 0
         self.reward = 0
-        self.episode_length = self.max_episode_length 
-        self.hand_open = False
+        self.episode_length = self.max_episode_length
         return self.state, self.phase
 
     def close(self):
         pass
-
-
 
--- src/q_learning/src/QLearnAgent.py
+++ src/q_learning/src/QLearnAgent.py
@@ -9,49 +9,54 @@ import rospy
 from CoLearnEnvironment import CoLearn
 
 
-class QLearningAgent():
+class QLearningAgent:
     def __init__(self, env):
         self.env = env
 
-        self.q_table = np.random.rand(self.env.state_size, self.env.action_size) * 0.01  # Random initialization of Q-table
+        # Random initialization of Q-table
+        self.q_table = np.random.rand(self.env.state_size, self.env.action_size) * 0.01
 
         self.reset_experience()
         self.state, self.phase = self.env.reset()
-        self.e_trace = np.zeros((self.env.observation_size, self.env.action_size))
+        self.e_trace = np.zeros((self.env.state_size, self.env.action_size))
 
-        self.initialise = True
-
-        self.alpha = 0.15
-        self.gamma = 0.8 
-        self.Lambda = 0.3
-        self.phase = 0
-
+        self.initialize = True
         self.type = 'none'
 
-        self.initialise_ros()
+        self.initialize_ros()
 
-    
-    def initialise_ros(self):
+    def initialize_ros(self):
         self.ros_running = rosgraph.is_master_online()
         if self.ros_running:
             try:
                 rospy.init_node('Q_agent', anonymous=True)
-            except:
-                rospy.logwarn("Cannot initialize node 'Q_agent' as it has already been initialized at the top level as a ROS node.")
+            except rospy.exceptions.ROSException:
+                rospy.logwarn(
+                    "Cannot initialize node 'Q_agent' as it has already been initialized."
+                )
         else:
             print("ROS is offline! Agent proceeds in offline mode")
 
-    def train(self, n_steps = 100000, learning_rate=0.8, discount_factor=0.8, exploration_factor=0.25, trace_decay=0.3,real_time=False):
-        '''
+    def train(
+        self,
+        n_steps=100000,
+        learning_rate=0.8,
+        discount_factor=0.8,
+        exploration_factor=0.25,
+        trace_decay=0.3,
+        real_time=False,
+    ):
+        """
         Trains the Q-learning agent.
-        :param n_steps: Number of training steps.
-        :param learning_rate: Learning rate for Q-learning.
-        :param discount_factor: Discount factor for future rewards.
-        :param exploration_factor: Exploration rate for epsilon-greedy policy.
-        :param trace_decay: Decay rate for eligibility traces.
-        :param real_time: If True, train in real-time mode.
-        '''
 
+        Args:
+            n_steps (int): Number of training steps.
+            learning_rate (float): Learning rate for Q-learning.
+            discount_factor (float): Discount factor for future rewards.
+            exploration_factor (float): Exploration rate for epsilon-greedy policy.
+            trace_decay (float): Decay rate for eligibility traces.
+            real_time (bool): If True, train in real-time mode.
+        """
         if self.type == 'follower':
             alpha = 0.5
         else:
@@ -59,41 +64,46 @@ class QLearningAgent():
 
         gamma = discount_factor
         epsilon = exploration_factor
-        Lambda = trace_decay
+        lamda = trace_decay
 
         if real_time:
-            if self.initialise:
+            if self.initialize:
                 self.state, self.phase = self.env.reset()
                 self.reset_experience()
                 terminated = False
                 self.e_trace = np.zeros_like(self.q_table)
-                self.initialise = False
+                self.initialize = False
 
             phase = self.phase
             valid = False
             while phase == self.phase and not valid:
                 action = self.epsilon_greedy(epsilon)
                 next_state, reward, terminated, info = self.env.step(action)
-         
-                phase = info.get('phase', phase)
-                valid = info.get('valid',False)
 
-                self.experience_update(self.state,action,next_state,reward,valid,phase)
+                phase = info.get('phase', phase)
+                valid = info.get('valid', False)
+
+                self.experience_update(
+                    self.state, action, next_state, reward, valid
+                )
 
                 if reward != 0 and valid:
-                    self.update_q_table(self.state, action, reward, next_state, alpha, gamma, Lambda) # To make sure the agent does not get stuck in a loop (closing hand when it is already closed)
+                    # To prevent the agent from getting stuck in a loop
+                    self.update_q_table(
+                        self.state, action, reward, next_state, alpha, gamma, lamda
+                    )
 
                 self.state = next_state
 
             self.phase = phase
 
             if terminated:
-                self.initialise = True
+                self.initialize = True
 
             return action, self.phase, terminated
 
         else:
-            for i in tqdm(range(1, n_steps + 1)):
+            for _ in tqdm(range(1, n_steps + 1)):
                 self.state, self.phase = self.env.reset()
                 self.reset_experience()
                 terminated = False
@@ -101,61 +111,68 @@ class QLearningAgent():
 
                 while not terminated:
                     action = self.epsilon_greedy(epsilon)
-
                     # Perform a step
                     next_state, reward, terminated, info = self.env.step(action)
-                    valid = info.get('valid',False)
+                    valid = info.get('valid', False)
                     self.phase = info.get('phase', self.phase)
-                    self.experience_update(self.state, action, next_state, reward,valid,self.phase)
+                    self.experience_update(
+                        self.state, action, next_state, reward, valid
+                    )
                     self.state = next_state
 
-                if i > 1:
-                    self.experience_replay(alpha, gamma, Lambda)
+                self.experience_replay(alpha, gamma, lamda)
 
-    def epsilon_greedy(self,epsilon):
-        # Exploration-exploitation trade-off
+    def epsilon_greedy(self, epsilon):
+        """Selects an action using epsilon-greedy policy."""
         if random.uniform(0, 1) < epsilon:
             action = self.env.action_space.sample()  # Explore action space
         else:
             action = np.argmax(self.q_table[self.state, :])  # Exploit learned values
-
         return action
 
-    def experience_update(self, state, action, next_state, reward,valid,phase):
+    def experience_update(self, state, action, next_state, reward, valid):
+        """Updates the experience buffer."""
         self.experience["state"].append(state)
         self.experience["action"].append(action)
         self.experience["next_state"].append(next_state)
         self.experience["reward"].append(reward)
         self.experience["valid"].append(valid)
 
-    def experience_replay(self, alpha, gamma, Lambda):
-        rospy.loginfo("updating Q-table with experience")
-        last_reward = self.experience["reward"][-1] 
+    def experience_replay(self, alpha, gamma, lamda):
+        """Updates the Q-table using experience replay."""
+        if self.ros_running:
+            rospy.loginfo("Updating Q-table with experience")
+
+        last_reward = self.experience["reward"][-1]
         for i in range(len(self.experience["state"])):
             state = self.experience["state"][i]
             action = self.experience["action"][i]
             next_state = self.experience["next_state"][i]
             valid = self.experience["valid"][i]
-            
-            if valid: # When a valid action is taken
-                reward = last_reward / self.env.phase_size # Increase the reward for this transition by the final reward per phase (if handover was successful this approach will reward the full trajectory)
-                self.update_q_table(state, action, reward, next_state, alpha, gamma, Lambda)
-                
-                                                           
+
+            if valid:
+                # Increase the reward for this transition by the final reward per phase
+                reward = last_reward / self.env.phase_size
+                self.update_q_table(state, action, reward, next_state, alpha, gamma, lamda)
+
     def reset_experience(self):
-        self.experience = {"state": [],
-                           "action": [],
-                           "next_state": [],
-                           "reward": [],
-                           "valid":[],
-                           "phase":[]}
+        """Resets the experience buffer."""
+        self.experience = {
+            "state": [],
+            "action": [],
+            "next_state": [],
+            "reward": [],
+            "valid": [],
+        }
 
     def reset(self):
+        """Resets the agent's state."""
         self.reset_experience()
         self.state, self.phase = self.env.reset()
         return self.state, self.phase
 
-    def update_q_table(self, state, action, reward, next_state, alpha, gamma, Lambda):
+    def update_q_table(self, state, action, reward, next_state, alpha, gamma, lamda):
+        """Updates the Q-table using the Q-learning update rule with eligibility traces."""
         old_value = self.q_table[state, action]
         next_max = np.max(self.q_table[next_state, :])
         td_error = reward + gamma * next_max - old_value
@@ -164,14 +181,16 @@ class QLearningAgent():
 
         self.q_table += alpha * td_error * self.e_trace
 
-        self.e_trace *= gamma * Lambda
+        self.e_trace *= gamma * lamda
 
-    
-    def save_q_table(self, directory="co_learning_robot_personalities/src/q_learning/Q_tables", prefix="q_table_"):
-        # Create the directory if it doesn't exist
+    def save_q_table(
+        self,
+        directory="co_learning_robot_personalities/src/q_learning/Q_tables",
+        prefix="q_table_",
+    ):
+        """Saves the Q-table to a file."""
         os.makedirs(directory, exist_ok=True)
 
-        # Find the next available filename
         index = 1
         while True:
             filename = f"{prefix}{index}.npy"
@@ -180,40 +199,51 @@ class QLearningAgent():
                 break
             index += 1
 
-        # Save the data to the file
-        with open(filepath, "wb") as file:
-            np.save(file, self.q_table)
+        np.save(filepath, self.q_table)
 
         if self.ros_running:
             rospy.loginfo(f"Saved Q-table as: {filepath}")
         else:
             print(f"Saved Q-table as: {filepath}")
 
-    def load_q_table(self, directory="co_learning_robot_personalities/src/q_learning/Q_tables/q_table_solved_1000000_1"):
+    def load_q_table(
+        self, filepath="co_learning_robot_personalities/src/q_learning/Q_tables/q_table.npy"
+    ):
+        """Loads the Q-table from a file."""
         try:
-            self.q_table = np.load(directory)
-            rospy.loginfo(f"Q_table loaded from directory: {directory}")
+            self.q_table = np.load(filepath)
+            if self.ros_running:
+                rospy.loginfo(f"Q-table loaded from: {filepath}")
+            else:
+                print(f"Q-table loaded from: {filepath}")
         except Exception as e:
-            rospy.logwarn(e)
+            if self.ros_running:
+                rospy.logwarn(f"Failed to load Q-table: {e}")
+            else:
+                print(f"Failed to load Q-table: {e}")
 
-    def print_q_table(self,margin=12):
-        print("Q-Table:")
-        header = "".join([f"{'Action ' + str(i):<{margin}}" for i in range(self.env.action_size)])
+    def print_q_table(self, margin=12):
+        """Prints the Q-table."""
+        header = "".join(
+            [f"{'Action ' + str(i):<{margin}}" for i in range(self.env.action_size)]
+        )
         print(f"{'':<{margin}}" + header)
         for state in range(self.env.state_size):
-            row = f"State {state:<{margin - 6}}"  # Adjust for 'State ' prefix length
-            row += "".join([f"{self.q_table[state, action]:<{margin}.2f}" for action in range(self.env.action_size)])
+            row = f"State {state:<{margin - 6}}"
+            row += "".join(
+                [
+                    f"{self.q_table[state, action]:<{margin}.2f}"
+                    for action in range(self.env.action_size)
+                ]
+            )
             print(row)
-        
+
+
 if __name__ == '__main__':
     try:
         n_steps = 10000
-        Agent = QLearningAgent(env=CoLearn())
-        Agent.train(n_steps=n_steps)
-        Agent.save_q_table(prefix=f"q_table_solved_{n_steps}_")
-        #Agent.load_q_table(directory="code_jesse/Q_tables/q_table_5.npy")
-        #Agent.evaluate()
+        agent = QLearningAgent(env=CoLearn())
+        agent.train(n_steps=n_steps)
+        agent.save_q_table(prefix=f"q_table_solved_{n_steps}_")
     except Exception as e:
         print(e)
-
-
